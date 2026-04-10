cab19a8c5abc7525235a17f37ee7ca56c25297a4
Jasmine <sandhujasmine@seasiainfotech.com>
2026-04-10 22:49:29 +0530

    project setup

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Jasmine Kaur on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
